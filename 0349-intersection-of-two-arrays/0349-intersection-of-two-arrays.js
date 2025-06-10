65e67fe20220f6b9ce5a186a504a9414c4ac4060
Harsh Chavan <103315332+theharshchavan@users.noreply.github.com>
2025-06-10 22:52:48 +0530

    Time: 1 ms (82.63%), Space: 54.3 MB (69.85%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const set1 = new Set(nums1);
+  const result = new Set();
+
+  for (const num of nums2) {
+    if (set1.has(num)) {
+      result.add(num);
+    }
+  }
+
+  return Array.from(result);
+};
